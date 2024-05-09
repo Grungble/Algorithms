4529106f2f62c0ee07eec46aeca8d99ab1c478c1
Grungble <118462674+Grungble@users.noreply.github.com>
2024-05-08 21:03:37 -0400

    Removed testers
    removed the print tests and delay

--- Sorting List.py
+++ Sorting List.py
@@ -7,20 +7,11 @@ num = 0
 while True:
     iteration = 0
     for x in unsorted_list:
-        print(x)
-    #sorted.append(i)
         if iteration < x:
             iteration = x
     if iteration >= all(unsorted_list): 
-        print(iteration)
         sorted_list.insert(0,iteration)
         unsorted_list.remove(iteration)
-        print(sorted_list)
-        print(unsorted_list)
-    time.sleep(1)
     if len(unsorted_list) == 0:
         break
-    #sorted_list.append(iteration)
-    #list.remove(i)
-    # print(sorted_list)
 print("list sorted", sorted_list)
