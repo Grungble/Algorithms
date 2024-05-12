b4e99c82f71cc9be70ca2f0c410a0dd1c765d992
Grungble <118462674+Grungble@users.noreply.github.com>
2024-05-12 12:36:23 -0400

    Marriage algo started
    started and edited marriage algo randomizer is done

--- Mairage Algo.py
+++ Mairage Algo.py
@@ -1,7 +1,7 @@
 import math
 import time
 a=0
-male1 = [2,1,3]
+male1 = [1,2,3]
 male2 = [1,3,2]
 male3 = [3,2,1]
 female1 = [1,3,2]
@@ -9,6 +9,7 @@ female2 = [1, 2, 3]
 female3 = [3,2,1]
 males = [male1, male2, male3]
 females = [female1, female2, female3]
+pairs= []
 m_num = 0
 f_num = 0
 # print(female1.index(1))
@@ -17,7 +18,7 @@ f_num = 0
 #     print("Male1 and Female1 are Compatible!")
 while (len(females))>0:
     for male_it, m1 in enumerate(males):
-        time.sleep(.75) 
+        #.sleep(.75) 
         for female_it, f1  in enumerate(females):
             if m1.index(1) == f1.index(1):
                 print("Pair Found", "Male",male_it+1+m_num, m1, "female",female_it+1+f_num, f1)
---
+++ Mariage Randomizer.py
@@ -0,0 +1,43 @@
+import time
+import math
+import random
+
+Num_people = 5
+Wives_list = []
+husbands_list = []
+husband_diction = {} #each wive's husband rankings
+Wife_Diction = {}
+match_diction = {}
+temp_dic = {}
+
+for i in range(Num_people):#generates the lists to be shuffled
+    Wives_list.append(f'w{i+1}')
+    husbands_list.append(f'h{i+1}')
+for en, i in enumerate(range(Num_people)): 
+    Wives_ranking = Wives_list.copy()
+    husbands_ranking = husbands_list.copy()
+    random.shuffle(Wives_ranking)
+    random.shuffle(husbands_ranking)
+    Wife_Diction.update({f'h{en+1}' :Wives_ranking}) #add husbands ranking of wives to dictionary
+    husband_diction.update({f'w{en+1}':husbands_ranking})#add wives ranking of husband to dictionary
+#pair people together
+#itterates wives list and husband rankings
+for h_key, h_value in (husband_diction.items()):
+    if not h_key in match_diction.keys():
+        for w_key,w_value in (Wife_Diction.items()):
+            if not w_key in match_diction.values():
+                match_diction.update({h_key:w_key})
+        # del (h_key, w_key)
+for wife, husband in (match_diction.items()):
+    temp_h = husband_diction.get(wife)#grabs rankings of husbands
+    temp_w = Wife_Diction.get(husband)#grabs rankings of wifes
+    #print(temp_w)
+    print(temp_h.index(husband))
+
+    if temp_h.index(husband) <1: #check husband rank
+        print(wife, "will not cheat on", husband)
+    if temp_w.index(wife) < 1:
+         print(husband, "will not cheat on",wife)
+    if temp_h.index(husband) <1 & temp_w.index(wife) < 1:
+        print(temp_h,"and",temp_w,"are stable") 
+    
