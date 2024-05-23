20df4c99961166ae7622c16e947125e57b33c931
Grungble <118462674+Grungble@users.noreply.github.com>
2024-05-22 20:01:45 -0400

    rank voting done
    interesting results for the thing

--- CSV matrix/matrix_mult.py
+++ CSV matrix/matrix_mult.py
@@ -1,29 +1,34 @@
 #multiply and transform matricies
 
-new_matrix = [
-    [1,2,3],
-    [3,4,2],
-    [2,4,1]
-]
-new_vector = [2,3,4]
+new_matrix = [[0, 1.0, 0.5, 0.25, 1.0], 
+              [0.5, 0, 0.5, 0.25, 0], 
+              [0.5, 0, 0, 0.25, 0], 
+              [0, 0, 0, 0, 0], 
+              [0, 0, 0, 0.25, 0]]
+
+
+new_vector = [2.75,1.25,.75,0,.25]
 transposed_matrix = []
 
 def matrix_trans(og_matrix:list):
     trans_matrix = []
-    trans_matrix = og_matrix.copy()
-    print(trans_matrix)
-    for row in trans_matrix:
-        for enum, val in enumerate(row):
-            print(val)
-        pass
-
+    for i in range(len(og_matrix)):
+        new_row = [0 for i in range(len(og_matrix))]
+        # print(new_row)
+        # print(i)
+        for enum, row in enumerate(og_matrix):
+            new_val = row.pop(0)
+            new_row[enum] = new_val
+        trans_matrix.append(new_row)
+        #print(trans_matrix)        
+    return(trans_matrix)
 def matrix_multi(matrix:list, vector:list):
     result_vector = []
     for row in matrix:
         temp_val = 0
         for m_val, v_val in zip(row, vector):
             temp_val = (m_val*v_val) + temp_val
-            print(result_vector)
+            #print(result_vector)
         result_vector.append(temp_val)
     print(result_vector)
     return result_vector
@@ -32,3 +37,6 @@ def matrix_multi(matrix:list, vector:list):
 
 if __name__ == '__main__':
     matrix_multi(new_matrix, new_vector)
+    transposed_matrix = matrix_trans(new_matrix)
+    print(transposed_matrix)
+    
--- CSV matrix/rank_voting.py
+++ CSV matrix/rank_voting.py
@@ -1,5 +1,7 @@
 import csv
 from os.path import dirname, realpath
+from matrix_mult import matrix_multi, matrix_trans
+from matplotlib import pyplot as plt
 DIR = dirname(realpath(__file__))
 graph_dict = {}
 graph_matrix = []
@@ -17,6 +19,7 @@ def read_graph_file(fname:str,graph_dict:dict):
                 adj_list = []
                 for ch in right_str.strip():
                     adj_list.append(ch)
+                adj_list.sort()
                 graph_dict[key_node] = adj_list
     return graph_dict
 
@@ -25,6 +28,7 @@ def converter(graph_dict:dict,matrix:list):
     for key in graph_dict:
         key_list.append(key)
     key_list.sort()
+    print('key_list',key_list)
     for key in (graph_dict):
         connections = graph_dict.get(key)
         new_row = []
@@ -37,25 +41,44 @@ def converter(graph_dict:dict,matrix:list):
             #print('key and new row',key,new_row)
         matrix.append(new_row)
     return matrix
-def voting(vote_matrix:list,voter_dict:dict):
-    voter_list = []
+
+def voting(vote_matrix:list):
     vote_value_list = []
-    initial_vote_val = 1
     #assumes order
-    for key in voter_dict:
-        voter_list.append(key)
     for vote_val in (vote_matrix):
         value = 1/sum(vote_val)
         vote_value_list.append(value)
-    print(vote_value_list)
-    #take the vote val and 
-    for i in zip():
-        pass
-    
+    for power , voter in zip(vote_value_list, vote_matrix):
+        for enum,i in enumerate(voter):
+            if i == 1:
+                voter[enum] = power  
+    print(graph_matrix)
+    trnf_matrix = matrix_trans(vote_matrix)
+    return trnf_matrix
+
+def calc_vote(matrix, vector,counter,limit,point):
+    points = []
+    while counter <= limit:
+        vector = matrix_multi(matrix,vector)
+        points.append(vector[point])
+        counter +=1
+    return points
+
 if __name__ == '__main__':
     VOTE_RANK_GRAPH = 'vote_rank.csv'
+    counter = 0
+    track = 1
+    max_lim = 1000
+    x_lim = [i for i in range(max_lim+1)]
+    plt.ylim(0,3)
     read_graph_file(VOTE_RANK_GRAPH, graph_dict)
     print(graph_dict)
     converter(graph_dict,graph_matrix)
     print(graph_matrix)
-    voting(graph_matrix,graph_dict)
+    trasn_matrix = voting(graph_matrix)
+    reputation = [1 for num in  range(len(trasn_matrix[0]))]
+    calc_vote(trasn_matrix,reputation,counter, max_lim, track)
+    for i in range(5):
+        plt.plot(x_lim,calc_vote(trasn_matrix,reputation,counter, max_lim, i))
+    plt.show()
+   
